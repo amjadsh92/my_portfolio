4182abab09a2d05a74239e87b255337b9faef26f
amjadsh92 <amjadsharafeddien@gmail.com>
2025-10-12 11:41:30 +0300

    add a light mode
    1- organise the scss code
    2- add a light mode icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,59 +1,47 @@
 /* eslint-disable */
-import { Menubar } from 'primereact/menubar';
-import './Navbar.scss'
-        
+import { Menubar } from "primereact/menubar";
+import "./Navbar.scss";
+import contrastIcon from "../../assets/images/contrast.png"; 
 
+function Navbar() {
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "About",
+      icon: "pi pi-user",
+    },
+    {
+      label: "Projects",
+      icon: "pi pi-folder",
+    },
+    {
+      label: "Game",
+      icon: "pi pi-box",
+    },
+    {
+      label: "Contact",
+      icon: "pi pi-envelope",
+    },
+    {
+    icon: "pi pi-sun"
+    }
+  ];
 
-function Navbar(){
+  const navbarName =<div className="navbar-leftside">
+    <span className="navbar-name">Amjad Sharafeddine</span>
+    <img src={contrastIcon} width="30" height="30" />
 
-    const items = [
-        {
-            label: 'Home',
-            icon: 'pi pi-home'
-        },
-        {
-            label: 'About',
-            icon: 'pi pi-user'
-        },
-        {
-            label: 'Projects',
-            icon: 'pi pi-folder',
-        },
-         {
-            label: 'Game',
-            icon: 'pi pi-box'
-        },
-        {
-            label: 'Contact',
-            icon: 'pi pi-envelope'
-        }
-    ];
+  </div> 
+  
 
-
-    const navbarName = (
-        
-            
-            <span className="navbar-name">Amjad Sharafeddine</span>
-        
-    );
-
-
-
-    return(
-        
-        <div className="navbar" >
-            <Menubar model={items} start={navbarName} />
-        </div>
-        
-    )
-
-
-
-
-
-
+  return (
+    <div className="navbar">
+      <Menubar model={items} start={navbarName}  />
+    </div>
+  );
 }
 
-
-
-export default Navbar
+export default Navbar;
