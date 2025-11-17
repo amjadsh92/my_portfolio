034b5e0430dcfc04565dbac1acb9d397847e9282
amjadsh92 <amjadsharafeddien@gmail.com>
2025-11-17 19:22:04 +0200

    don't let the highlight automatically change whn clicking on a nav item

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,134 +1,4 @@
-// /* eslint-disable */
-// import { Menubar } from "primereact/menubar";
-// import "./Navbar.scss";
-// import contrastIcon from "../../assets/images/contrast.png";
-// import { useState } from "react";
-// import { useContext } from "react";
-// import { PrimeReactContext } from "primereact/api";
 
-// function Navbar({isDarkMode, setIsDarkMode}) {
-  
-
-//   const [isClicked, setIsClicked] = useState({
-//     home: true,
-//     about: false,
-//     projects: false,
-//     game: false,
-//     contact: false,
-//   });
-
-//   const { changeTheme } = useContext(PrimeReactContext);
-
-//   const items = [
-//     {
-//       label: "Home",
-//       icon: "pi pi-home",
-//       command: () =>
-//         setIsClicked({
-//           about: false,
-//           projects: false,
-//           game: false,
-//           contact: false,
-//           home: true,
-//         }),
-//       className: `${isClicked.home ? "menuitem-clicked" : ""}`,
-//     },
-//     {
-//       label: "About",
-//       icon: "pi pi-user",
-//       command: () =>
-//         setIsClicked({
-//           about: true,
-//           projects: false,
-//           game: false,
-//           contact: false,
-//           home: false,
-//         }),
-//       className: `${isClicked.about ? "menuitem-clicked" : ""}`,
-//     },
-//     {
-//       label: "Projects",
-//       icon: "pi pi-folder",
-//       command: () =>
-//         setIsClicked({
-//           about: false,
-//           projects: true,
-//           game: false,
-//           contact: false,
-//           home: false,
-//         }),
-//       className: `${isClicked.projects ? "menuitem-clicked" : ""}`,
-//     },
-   
-//     {
-//       label: "Contact",
-//       icon: "pi pi-envelope",
-//       command: () =>
-//         setIsClicked({
-//           about: false,
-//           projects: false,
-//           game: false,
-//           contact: true,
-//           home: false,
-//         }),
-//       className: `${isClicked.contact ? "menuitem-clicked" : ""}`,
-//     },
-
-//     {
-//       template: (item, options) => {
-//         const handleClick = (e) => {
-//           e.stopPropagation();
-//           e.preventDefault();
-
-//           changeTheme(
-//             isDarkMode ? "lara-dark-indigo" : "lara-light-indigo",
-//             isDarkMode ? "lara-light-indigo" : "lara-dark-indigo",
-//             "theme-link"
-//           );
-          
-          
-//           setIsDarkMode((prev) => !prev);
-//           localStorage.setItem("isDarkMode", !isDarkMode);
-//         };
-
-//         return (
-//           <button
-//             className="p-link theme-toggle-btn"
-//             onClick={handleClick}
-//             type="button"
-//             aria-label="Toggle theme"
-//           >
-//             <div className="lightmode-block">
-//               <i
-//                 className={`${isDarkMode ? "darkmode-icon pi pi-moon" : "lightmode-icon pi pi-sun"}`}
-//               />{" "}
-//               <span
-//                 className={`${isDarkMode ? "darkmode-text" : "lightmode-text"}`}
-//               >
-//                 {isDarkMode ? "Dark mode" : "Light mode"}
-//               </span>
-//             </div>
-//           </button>
-//         );
-//       },
-//     },
-//   ];
-
-//   const navbarName = (
-//     <div className="navbar-leftside">
-//       <span className="navbar-name">Amjad Sharafeddine</span>
-//       <img  className="navbar-avatar" src={contrastIcon} width="30" height="30" />
-//     </div>
-//   );
-
-//   return (
-//     <div className={`navbar ${isDarkMode ? "" : "navbar-light"}`}>
-//       <Menubar model={items} start={navbarName} />
-//     </div>
-//   );
-// }
-
-// export default Navbar;
 
 /* eslint-disable */
 import { Menubar } from "primereact/menubar";
@@ -142,8 +12,9 @@ function Navbar({ isDarkMode, setIsDarkMode, refs }) {
   const { changeTheme } = useContext(PrimeReactContext);
 
   const [activeSection, setActiveSection] = useState("home");
+  const [isClicked, setIsClicked] = useState(false)
 
-  // 🔥 IntersectionObserver logic
+  
   useEffect(() => {
     const sections = [
       { id: "home", ref: homeRef },
@@ -159,9 +30,19 @@ function Navbar({ isDarkMode, setIsDarkMode, refs }) {
             const current = sections.find(
               (s) => s.ref.current === entry.target
             );
-            if (current) {
-              setActiveSection(current.id);
+            if(current && activeSection === current.id){
+               setIsClicked(false)
+              //  setActiveSection(current.id);
             }
+            
+            else if (current && !isClicked ) {
+              
+              setActiveSection(current.id);
+
+            }
+           
+           
+            
           }
         });
       },
@@ -173,60 +54,82 @@ function Navbar({ isDarkMode, setIsDarkMode, refs }) {
     });
 
     return () => observer.disconnect();
-  }, [homeRef, aboutRef, projectsRef, contactRef]);
+  }, [homeRef, aboutRef, projectsRef, contactRef, activeSection, isClicked]);
 
-  // 🔥 Highlight menu item by checking activeSection
+
   const items = [
     {
       label: "Home",
       icon: "pi pi-home",
-      command: () => homeRef.current.scrollIntoView({ behavior: "smooth" }),
+      command: () => 
+        { 
+          setIsClicked(true) 
+          
+          homeRef.current.scrollIntoView({ behavior: "smooth" })
+          
+          setActiveSection("home")
+          
+        }
+      ,
+ 
+                    
       className: activeSection === "home" ? "menuitem-clicked" : "",
     },
     {
       label: "About",
       icon: "pi pi-user",
-      command: () => aboutRef.current.scrollIntoView({ behavior: "smooth" }),
-      className: activeSection === "about" ? "menuitem-clicked" : "",
+      command: () => { 
+        
+         setIsClicked(true) 
+          
+          aboutRef.current.scrollIntoView({ behavior: "smooth" })
+          
+          setActiveSection("about")
+          }
+          
+        
+      
+          
+      ,
+      className: (activeSection === "about") ? "menuitem-clicked" : "",
     },
     {
       label: "Projects",
       icon: "pi pi-folder",
-      command: () => projectsRef.current.scrollIntoView({ behavior: "smooth" }),
-      className: activeSection === "projects" ? "menuitem-clicked" : "",
+      command: () =>  
+        { 
+          
+          setIsClicked(true) 
+          
+          projectsRef.current.scrollIntoView({ behavior: "smooth" })
+          
+          setActiveSection("projects")
+          
+          
+          
+        },
+      className: (activeSection === "projects") ? "menuitem-clicked" : "",
     },
     {
       label: "Contact",
       icon: "pi pi-envelope",
-      command: () => contactRef.current.scrollIntoView({ behavior: "smooth" }),
-      className: activeSection === "contact" ? "menuitem-clicked" : "",
+      command: () => {
+          
+       
+          setIsClicked(true) 
+          
+          contactRef.current.scrollIntoView({ behavior: "smooth" })
+          
+          setActiveSection("contact")
+          
+        
+          
+      
+      },
+      className: (activeSection === "contact" ) ? "menuitem-clicked" : "",
     },
     {
-  //     template: () => (
-  //       <button
-  //         className="p-link theme-toggle-btn"
-  //         onClick={() => {
-  //           changeTheme(
-  //             isDarkMode ? "lara-dark-indigo" : "lara-light-indigo",
-  //             isDarkMode ? "lara-light-indigo" : "lara-dark-indigo",
-  //             "theme-link"
-  //           );
-
-  //           setIsDarkMode((prev) => !prev);
-  //           localStorage.setItem("isDarkMode", !isDarkMode);
-  //         }}
-  //       >
-  //         <i className={isDarkMode ? "pi pi-moon" : "pi pi-sun"} />
-  //       </button>
-  //     ),
-  //   },
-  // ];
-
-  // return (
-  //   <div className={`navbar ${isDarkMode ? "" : "navbar-light"}`}>
-  //     <Menubar model={items} />
-  //   </div>
-  // );
+ 
    
       template: (item, options) => {
         const handleClick = (e) => {
