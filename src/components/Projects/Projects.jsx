8d978005abdfd9b45131d6fbf7ee04aba129d667
amjadsh92 <amjadsharafeddien@gmail.com>
2025-11-25 12:38:20 +0200

    choose a color for the card's backround

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,11 +17,11 @@ function Projects({ isDarkMode }) {
        <div className="project-description">
         <div className="project-name">URL Shortener</div>
         <br/><br/>
-        Family owned online business designed for one of my clients to sell CBD products in the Amsterdam market. The website provides an appealing and user-friendly interface for customers who seek the benefits of CBD products.<br/><br/>
+        <p className="description-text">Family owned online business designed for one of my clients to sell CBD products in the Amsterdam market. The website provides an appealing and user-friendly interface for customers who seek the benefits of CBD products.<br/><br/>
 
 It ensures that purchasing legalized CBD products is just a click away, catering to customers in both Japan and Amsterdam.<br/><br/>
 
-With a focus on aesthetics and usability, the website offers an engaging experience for users, making it easy for them to find and purchase the products they need.
+With a focus on aesthetics and usability, the website offers an engaging experience for users, making it easy for them to find and purchase the products they need.</p>
 
 
  
