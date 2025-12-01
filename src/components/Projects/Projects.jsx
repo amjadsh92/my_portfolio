1a669d4445944713e5dde0bc432e0b3eb0ba5f14
amjadsh92 <amjadsharafeddien@gmail.com>
2025-12-01 16:32:13 +0200

    make some changes in design 1- Decrease left padding in the project-description 2- Change "features" and "Tech" span elements into divs 3- Apply margin-top to them 4- Rephrase some features

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,118 +7,104 @@ import scssLogo from "../../assets/images/scss.png";
 import postgreLogo from "../../assets/images/postgre3.png";
 import prismaLogo from "../../assets/images/prismaORM.png";
 import primeReactLogo from "../../assets/images/primereact.png";
-import passportJSLogo from "../../assets/images/passportLogo.png"
+import passportJSLogo from "../../assets/images/passportLogo.png";
 import { Image } from "primereact/image";
-import { Button } from 'primereact/button';
+import { Button } from "primereact/button";
 
 function Projects({ isDarkMode }) {
- 
-
   return (
     <div className="projects-light">
-       <div className="projects-title">
-         <div className="title">projects</div>
-       </div>
-       <div className="projects-container">
-       
-       <div className="project-description-image">
-       <div className="project-image"><Image  className="shortURL-image" /> </div>
-       <div className="project-description">
-        <div className="project-name">
-          {/* <span className="projectNumber">1.{'  '}</span> */}
-          URL Shortener</div>
-        <br/><br/>
-       
+      <div className="projects-title">
+        <div className="title">projects</div>
+      </div>
+      <div className="projects-container">
+        <div className="project-description-image">
+          <div className="project-image">
+            <Image className="shortURL-image" />{" "}
+          </div>
+          <div className="project-description">
+            <div className="project-name">
+              {/* <span className="projectNumber">1.{'  '}</span> */}
+              URL Shortener
+            </div>
+            <br />
+            <br />
 
-<p className="description-text">
-  Full-Stack React app that transforms a long URL into a custom or auto-generated short URL.
-</p>
+            <p className="description-text">
+              Full-Stack React app that transforms a long URL into a custom or
+              auto-generated short URL.
+            </p>
 
- <div className="features"><i className="pi pi-list"></i> Features:</div>
+            <div className="features">
+              <i className="pi pi-list"></i> Features:
+            </div>
 
-<ul className="features-list">
-  {/* <li>Ability to create auto-generated short URLs</li>
-  <li>Ability to create custom short URLs (for registered users)</li>
-  <li>Session-based authentication</li>
-  <li>Fully responsive</li> */}
-  <li>Creates randomly generated short URLs</li>
-  <li>Creates custom short URLs (available for registered users)</li>
-  <li>Session-based authentication system</li>
-  <li>Fully responsive design across all devices</li>
-  <li>Personal dashboard where users can view and manage their saved short URLs</li>
+            <ul className="features-list">
+              <li>Creates randomly generated short URLs</li>
+              <li>
+                Creates custom short URLs (available for registered users)
+              </li>
+              <li>Session-based authentication system</li>
+              <li>Fully responsive design across all devices</li>
+              <li>
+                Personal dashboard where users can view and manage their saved
+                short URLs
+              </li>
+            </ul>
 
+            <div className="Tech">
+              <i className="pi pi-cog"></i>Tech used:
+            </div>
 
-</ul>
+            <div className="techs">
+              <div className="tech">
+                <img src={reactLogo} className="tech-image" />
+                <p className="tech-title">React</p>
+              </div>
+              <div className="tech">
+                <img src={expressLogo} className="tech-image" />
+                <p className="tech-title">ExpressJS</p>
+              </div>
+              <div className="tech">
+                <img src={scssLogo} className="tech-image" />
+                <p className="tech-title">SCSS</p>
+              </div>
+              <div className="tech">
+                <img src={postgreLogo} className="tech-image" />
+                <p className="tech-title">Postgres</p>
+              </div>
+              <div className="tech">
+                <img src={prismaLogo} className="tech-image" />
+                <p className="tech-title">Prisma</p>
+              </div>
+              <div className="tech">
+                <img src={primeReactLogo} className="tech-image" />
+                <p className="tech-title">PrimeReact</p>
+              </div>
+              <div className="tech">
+                <img src={passportJSLogo} className="tech-image" />
+                <p className="tech-title">Passport.js</p>
+              </div>
+            </div>
 
-<div className="Tech"><i className="pi pi-cog"></i>Tech used:</div>
-
-<div className="techs">
-  <div className="tech">
-    <img src={reactLogo} className="tech-image" />
-    <p className="tech-title">React</p>
-  </div>
-  <div className="tech">
-    <img src={expressLogo} className="tech-image" />
-    <p className="tech-title">ExpressJS</p>
-  </div>
-  <div className="tech">
-    <img src={scssLogo} className="tech-image" />
-    <p className="tech-title">SCSS</p>
-  </div>
-  <div className="tech">
-    <img src={postgreLogo} className="tech-image" />
-    <p className="tech-title">Postgres</p>
-  </div>
-  <div className="tech">
-    <img src={prismaLogo} className="tech-image" />
-    <p className="tech-title">Prisma</p>
-  </div>
-  <div className="tech">
-    <img src={primeReactLogo} className="tech-image" />
-    <p className="tech-title">PrimeReact</p>
-  </div>
-    <div className="tech">
-    <img src={passportJSLogo} className="tech-image" />
-    <p className="tech-title">Passport.js</p>
-  </div>
-
-   
-</div>
-{/* <span className="Tech"> More Details:</span><br /> */}
-<div className="readMore">
-{/* <i className="pi pi-github github" ></i>
-<i className="pi pi-external-link demo" ></i> */}
- {/* <Button icon="pi pi-github" className="github"  />  */}
- {/* <i className="pi pi-github github" ></i> */}
- {/* <Button icon="pi pi-github" className="github"  />
- <Button icon="pi pi-external-link" className="demo" />  */}
-  {/* <i className="pi pi-github github" ></i>
-  <i className="pi pi-external-link demo" ></i> */}
-  <Button
-        
-        label="Show demo"
-        icon="pi pi-external-link"
-        iconPos="right"
-        className="demo"
-        
-      />
-   <Button
-        
-        label="Read more"
-        icon="pi pi-github"
-        iconPos="right"
-        className="github"
-        
-      />    
-</div>
-
-
- 
-</div>
-      </div> 
-       </div>
-      
-      
-    </div>)
+            <div className="readMore">
+              <Button
+                label="Show demo"
+                icon="pi pi-external-link"
+                iconPos="right"
+                className="demo"
+              />
+              <Button
+                label="Read more"
+                icon="pi pi-github"
+                iconPos="right"
+                className="github"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Projects
+export default Projects;
