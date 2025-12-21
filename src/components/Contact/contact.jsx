15d9f85a6c80659f31fc366994f5ffe9c6dab165
amjadsh92 <amjadsharafeddien@gmail.com>
2025-12-21 07:25:43 +0200

    complete the form design

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -3,23 +3,24 @@ import "./contact.scss";
 import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { FloatLabel } from "primereact/floatlabel";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Button } from "primereact/button";
 
 
 function Contact({ isDarkMode }) {
  
    const [name, setName] = useState('');
+  
+   const [email, setEmail] = useState('');
+
+   const [message, setMessage] = useState('')
 
   return (
     <div  className="contact">
       <div className="contact-title">
           Contact
       </div>
-      <div className="contact-subtitle">
-
-        Let's get in touch
-
-
-      </div>
+      
 
       <div className="contact-text">
 
@@ -28,11 +29,31 @@ function Contact({ isDarkMode }) {
       </div >
 
       <div className="contact-form">
+
+        <div className="form-title">
+
+        Let's get in touch!
+        </div>
         
-           <FloatLabel className="username">
-                <InputText id="username" value={name} onChange={(e) => setName(e.target.value)} />
-                <label > Username </label>
+           <FloatLabel className="name">
+                <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <label htmlFor="name" > Name </label>
           </FloatLabel>
+
+           <FloatLabel className="email">
+                <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label htmlFor="email" > Email </label>
+          </FloatLabel>
+
+          <FloatLabel className="message">
+          <InputTextarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} rows={5} cols={30} />
+          <label htmlFor="message">Your Message</label>
+         </FloatLabel>
+         <Button
+          className="contact-submit"
+          label="Submit"
+          severity="info"
+          />
  
       </div>
 
