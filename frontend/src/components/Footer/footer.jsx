5381d3c772eea9a4297c686429bff643f287d997
amjadsh92 <amjadsharafeddien@gmail.com>
2026-01-06 20:54:16 +0200

    implement responsivity for the footer

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -6,10 +6,10 @@ function Footer({ isDarkMode }) {
    
 
     return(
-     <div className="footer">
+     <div className={`${isDarkMode ? "footer-dark" : "footer-light"}`}>
         <div className="footer-text">
         <div className="footer-rights">&copy; 2026,&nbsp;<span className="footer-name">Amjad Sharafeddine</span>.{" "}All rights reserved.</div>
-        <div className="footer-follow">Follow me on <i className="pi pi-github github" ></i> <i className="pi pi-linkedin linkedin" ></i> 
+        <div className="footer-follow">Follow me on &nbsp; <i className="pi pi-github github" ></i> &nbsp;<i className="pi pi-linkedin linkedin" ></i> 
         
         </div>
       </div>
