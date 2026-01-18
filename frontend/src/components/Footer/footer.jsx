e117d1a85f6fa21c89b3c65e8da9933284a0d83b
amjadsh92 <amjadsharafeddien@gmail.com>
2026-01-18 13:30:47 +0200

    handle responsivity at large screens

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -1,44 +1,36 @@
 /* eslint-disable */
 import "./footer.scss";
 
-
 function Footer({ isDarkMode }) {
-   
-
-    return(
-     <div className={`${isDarkMode ? "footer-dark" : "footer-light"}`}>
-        <div className="footer-text">
-        <div className="footer-rights">&copy; {new Date().getFullYear()},&nbsp;<span className="footer-name">Amjad Sharafeddine</span>.{" "}All rights reserved.</div>
-        <div className="footer-follow">Follow me on &nbsp; 
-          
-          {/* <i className="pi pi-github github" href="" ></i> &nbsp;<i className="pi pi-linkedin linkedin" href="" ></i>  */}
-
+  return (
+    <div className={`${isDarkMode ? "footer-dark" : "footer-light"}`}>
+      <div className="footer-text">
+        <div className="footer-rights">
+          &copy; {new Date().getFullYear()},&nbsp;
+          <span className="footer-name">Amjad Sharafeddine</span>. All rights
+          reserved.
+        </div>
+        <div className="footer-follow">
+          Follow me on &nbsp;
           <a
-  href="https://github.com/amjadsh92"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <i className="pi pi-github github"></i>
-</a>
-
-&nbsp;&nbsp;
-
-<a
-  href="https://www.linkedin.com/in/amjad-sharafeddine/"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <i className="pi pi-linkedin linkedin"></i>
-</a>
-        
+            href="https://github.com/amjadsh92"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="pi pi-github github"></i>
+          </a>
+          &nbsp;&nbsp;
+          <a
+            href="https://www.linkedin.com/in/amjad-sharafeddine/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="pi pi-linkedin linkedin"></i>
+          </a>
         </div>
       </div>
-     </div>
-
-    )
-    
-  ;
+    </div>
+  );
 }
 
 export default Footer;
-
